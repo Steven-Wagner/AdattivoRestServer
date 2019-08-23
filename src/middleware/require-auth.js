89ce575af704301b0597f6caedca4b843bb7f940
Steven Wagner <steven.glen.wagner@gmail.com>
2019-08-23 15:29:43 -0600

    Addec comments

--- src/middleware/require-auth.js
+++ src/middleware/require-auth.js
@@ -1,6 +1,8 @@
 function requireAuth(req, res, next) {
+    //get the password from the Authorization header
     const submittedPassword = req.get('Authorization') || '';
-        
+    
+    //If supplied password != the password stored in the .env file return 401
     if (submittedPassword !== process.env.DELETE_EMPLOYEE_PASSWORD) {
         res.status(401).json({
             message: 'Unauthorized request'
